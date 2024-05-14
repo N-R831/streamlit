0d9cc4db757a8f17e28f234d0ce6069f6afb14aa
R-Nishitsuji <ryohmaorama@gmail.com>
2024-05-14 22:09:44 +0900

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,62 +5,62 @@ import pandas as pd
 
 dbname = 'MASTER.db'
 
+selected_data = st.sidebar.selectbox('メニュー', ['Edit', 'Record'])
 
-date = st.date_input('日付を入力してください。', datetime.date.today())
-input_num = st.number_input('回数を入力してください', value=0)
+if selected_data == 'Edit':
+    date = st.date_input('日付を入力してください。', datetime.date.today())
+    input_num = st.number_input('回数を入力してください', value=0)
 
-# 平均値の取得
-conn = sqlite3.connect(dbname)
+    # 平均値の取得
+    conn = sqlite3.connect(dbname)
 
-# テーブルを作成する 
-conn.execute('''
-CREATE TABLE IF NOT EXISTS master_dt ( 
-    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    date TEXT NOT NULL,
-    master_num INTEGER NOT NULL
-) 
-''')
+    # テーブルを作成する 
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS master_dt ( 
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        date TEXT NOT NULL,
+        master_num INTEGER NOT NULL
+    ) 
+    ''')
 
-str_sql = 'SELECT avg(master_num) FROM master_dt'
-# sqliteを操作するカーソルオブジェクトを作成
-cur = conn.cursor()
-cur.execute(str_sql)
-ret = cur.fetchall()
-ave = ret[0]
-st.write('Average: ', ave)
-
-if st.button('start'):
-    date_str = date.strftime('%Y-%m-%d')
-    result = input_num
-    # 存在チェック
-    df = pd.read_sql(f'''
-        SELECT master_num
-        FROM master_dt
-        WHERE date = "{date_str}"
-        ''', conn)
-    if not df.empty:
-        str_sql = "UPDATE  master_dt SET master_num=" + f'{result}'+ " WHERE DATE=" + f"'{date_str}'"
-    else:
-        str_sql = "INSERT INTO master_dt (date, master_num) values(" + f"'{date_str}'" + ", " + f'{result}' + ")"
+    str_sql = 'SELECT avg(master_num) FROM master_dt'
+    # sqliteを操作するカーソルオブジェクトを作成
+    cur = conn.cursor()
     cur.execute(str_sql)
-    
-    # データベースへコミット。これで変更が反映される。
-    conn.commit()
+    ret = cur.fetchall()
+    ave = ret[0]
 
-# 統計データの表示
-df = pd.read_sql(f'''
-SELECT date, master_num
-FROM master_dt
-ORDER BY date DESC
-''', conn)
+    if st.button('submit'):
+        date_str = date.strftime('%Y-%m-%d')
+        result = input_num
+        # 存在チェック
+        df = pd.read_sql(f'''
+            SELECT master_num
+            FROM master_dt
+            WHERE date = "{date_str}"
+            ''', conn)
+        if not df.empty:
+            str_sql = "UPDATE  master_dt SET master_num=" + f'{result}'+ " WHERE DATE=" + f"'{date_str}'"
+        else:
+            str_sql = "INSERT INTO master_dt (date, master_num) values(" + f"'{date_str}'" + ", " + f'{result}' + ")"
+        cur.execute(str_sql)
+        
+        # データベースへコミット。これで変更が反映される。
+        conn.commit()
+elif selected_data == 'Record':
+    # 統計データの表示
+    df = pd.read_sql(f'''
+    SELECT date, master_num
+    FROM master_dt
+    ORDER BY date DESC
+    ''', conn)
 
 
-if not df.empty:
-    st.header('平均')
+    if not df.empty:
+        st.header('平均')
+        avg_num = df['master_num'].mean()
 
-avg_num = df['master_num'].mean()
-
-st.write(f'{avg_num:.1f}')
-st.dataframe(df)
+        st.write(f'{avg_num:.3f}')
+        st.dataframe(df)
 
 #conn.close()
